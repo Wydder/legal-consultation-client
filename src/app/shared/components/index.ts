b3fb47bb16236c8da577a1f911159b650008280d
wydder <bogdan.mosica@gmail.com>
2019-06-30 17:26:54 +0300

    Created left menu component

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,1 +1,7 @@
-export const components: any[] = [];
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+
+export const components: any[] = [
+  LeftMenuComponent,
+  TopBarComponent,
+];

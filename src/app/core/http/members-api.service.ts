bc756021247b27fd129c5e7172df43c37cde9d1d
wydder <bogdan.mosica@gmail.com>
2019-07-19 18:39:39 +0300

    Added htpp call

--- src/app/core/http/members-api.service.ts
+++ src/app/core/http/members-api.service.ts
@@ -12,70 +12,12 @@ import { Member, MemberResponse } from '../models';
   providedIn: 'root',
 })
 export class MembersApiService {
-  aMember1: MemberResponse = {
-    id: 1,
-    email: 'abasdfsdfds@adsasdas',
-    firstName: 'basddsasdas',
-    lastName: 'basdfsdfdsdasdsasdas',
-    phoneNumber: '0456254115',
-    district: 'asdasdasddas',
-    organisation: 'organisation',
-  };
-  aMember2: MemberResponse = {
-    id: 2,
-    email: 'sbasdfsdfds@adsasdas',
-    firstName: 'hbasddsasdas',
-    lastName: 'basdfsdfdsdasdsasdas',
-    district: 'asdasdasddas',
-    phoneNumber: '0456254115',
-    organisation: 'organisation',
-  };
-  aMember3: MemberResponse = {
-    id: 3,
-    email: 'fbasdfsdfds@adsasdas',
-    firstName: 'gbasddsasdas',
-    lastName: 'basdfsdfdsdasdsasdas',
-    phoneNumber: '0456254115',
-    district: 'asdasdasddas',
-    organisation: 'organisation',
-  };
-  aMember4: MemberResponse = {
-    id: 4,
-    email: 'gbasdfsdfds@adsasdas',
-    firstName: 'fbasddsasdas',
-    phoneNumber: '0456254115',
-    lastName: 'basdfsdfdsdasdsasdas',
-    district: 'asdasdasddas',
-    organisation: 'organisation',
-  };
-  aMember5: MemberResponse = {
-    id: 5,
-    email: 'cbasdfsdfds@adsasdas',
-    firstName: 'sbasddsasdas',
-    phoneNumber: '0456254115',
-    lastName: 'basdfsdfdsdasdsasdas',
-    district: 'asdasdasddas',
-    organisation: 'organisation',
-  };
-  aMember6: MemberResponse = {
-    id: 6,
-    email: 'basdfsdfds@adsasdas',
-    firstName: 'abasddsasdas',
-    phoneNumber: '0456254115',
-    lastName: 'basdfsdfdsdasdsasdas',
-    district: 'asdasdasddas',
-    organisation: 'organisation',
-  };
-
   constructor(private http: HttpClient) { }
 
   public list(): Observable<MemberResponse[]> {
-    const list = [this.aMember1, this.aMember2, this.aMember3, this.aMember4, this.aMember5, this.aMember6]
-    return of(list);
-
-    // return this.http.get<MemberResponse[]>(environment.api_url + '/users')
-    //   .pipe(
-    //     catchError(aError:> observableThrowError(aError))
-    //   );
+    return this.http.get<MemberResponse[]>(environment.api_url + '/users')
+      .pipe(
+        catchError(aError => observableThrowError(aError))
+      );
   }
 }

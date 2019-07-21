85f33b76b8dcc2c24096ed1567283b84b5246add
wydder <bogdan.mosica@gmail.com>
2019-07-21 21:07:21 +0300

    Added translation to top bar

--- src/app/authentication/containers/log-in/log-in.component.ts
+++ src/app/authentication/containers/log-in/log-in.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import {
   // Services
